7989890dba98dc28f622d7d969d5e985f29dc2f1
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-15 17:24:43 +0300

    05. метод some

---
+++ 05. метод some/js/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Метод some для проверки массива в JavaScript
+
+// №1⊗jsPmEnmSm
+// Дан массив с числами. Проверьте то, что в массиве есть хотя бы одно число больше нуля.
+/* let arr = [2, 4, 5, 6, 7, 8];
+let result = arr.some(function(elem){
+	return elem > 0;
+});
+console.log(result); */ //true;
+
+/* №2⊗jsPmEnmSm
+Дан массив с числами. Проверьте то, что хотя бы для одного элемента произведение его значения на порядковый номер больше 30. */
+/* let arr = [2, 4, 5, 6, 7, 8, 35, 21];
+let result = arr.some(function (elem, index) {
+	return elem * index > 30
+});
+console.log(result); */ //true
+
